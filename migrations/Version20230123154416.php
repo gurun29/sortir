a81a24182b89c3e0ea0d2843d9732d2b0ece6d04
gurun29 <98386819+gurun29@users.noreply.github.com>
2023-01-23 16:51:32 +0100

    Premier commit

---
+++ migrations/Version20230123154416.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20230123154416 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE participant ADD pseudo VARCHAR(50) NOT NULL');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_D79F6B115126AC48 ON participant (mail)');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_D79F6B1186CC499D ON participant (pseudo)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP INDEX UNIQ_D79F6B115126AC48 ON participant');
+        $this->addSql('DROP INDEX UNIQ_D79F6B1186CC499D ON participant');
+        $this->addSql('ALTER TABLE participant DROP pseudo');
+    }
+}
