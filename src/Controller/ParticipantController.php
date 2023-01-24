bd50f7dfa6a2b37b5bad646dfc21a9f32a65ffab
gurun29 <98386819+gurun29@users.noreply.github.com>
2023-01-24 19:37:30 +0100

    page mon profil (à finaliser)

--- src/Controller/ParticipantController.php
+++ src/Controller/ParticipantController.php
@@ -8,48 +8,91 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
-
+use Symfony\Component\Security\Http\Authentication\UserAuthenticatorInterface;
 
 class ParticipantController extends AbstractController
 {
     /**
      * @Route("/mon_profil/{id}", name="mon_profil")
      */
-    public function modifier(Request $request, EntityManagerInterface $entityManager, ParticipantRepository $participantRepository, int $id): Response
+    public function modifier(
+        Request $request,
+        //UserAuthenticatorInterface $userAuthenticator,
+        UserPasswordHasherInterface $passwordHasher,
+        EntityManagerInterface $entityManager,
+        ParticipantRepository $participantRepository,
+        int $id
+    ): Response
     {
+        $MDP = "****";
+        $mdphash="";
         //$monProfil = new Participant();
         $monProfil = $participantRepository->find($id);
         if (!$monProfil){
             throw $this->createNotFoundException("oh no, the serie don't exist");
         }
-
+        //$monProfil->setPassword($MDP);
         $monProfilForm = $this->createForm(ParticipantType::class,$monProfil);
+        //$monProfilForm->get('password')->setData($MDP);
+        $monProfilForm->get('mdp')->setData($MDP);
+        $monProfilForm->get('mdp2')->setData($MDP);
+        //$monProfilForm->get('mdp')->add("test");
         //$monProfilForm = $this->createForm(Participant::class,$monProfil);
         //dump($monProfilForm);
         //dd($monProfil);
+        //$monProfilForm->add($passwordHasher->hashPassword(
+        //    $monProfil,
+        //    $monProfilForm->get('password')->getData();
+        //));
 
+        //$monProfilForm = $this->renderForm();
 
         $monProfilForm->handleRequest($request);
 
-        if ($monProfilForm->isSubmitted() && $monProfilForm->isValid())
+        if ($monProfilForm->isSubmitted() && $monProfilForm->isValid()
+             && $monProfilForm->get('mdp')->getData() === $monProfilForm->get('mdp2')->getData())
         {
+            //dump($monProfil);
+            if ($monProfilForm->get('mdp')->getData() <> $MDP) {
+                //dump($monProfil);
+                $mdphash=$monProfilForm->get('mdp')->getData();
+                //dump($mdphash);
+
+                dd($passwordHasher);
+                $monProfil->setPassword(
+                    $passwordHasher->hashPassword(
+                        $monProfil,
+                        $monProfilForm->get('mdp')->getData()
+                    )
+                );
+                //$monProfil->setPassword($monProfilForm->get('mdp')->getData());
+                //dump($passwordHasher);
+                //dd($monProfil);
+            }
+            else {
+
+            }
+
+
+
+
+            dump($monProfil);
+
             //dd($monProfil);
             $entityManager->persist($monProfil);
             //dd($monProfil);
             $entityManager->flush();
             $this->addFlash('sucess','profil modifié');
-            //return $this->redirectToRoute('main');
+            return $this->redirectToRoute('main');
         }
 
-          return $this->render('participant/modifier.html.twig', [
+        return $this->render('participant/modifier.html.twig', [
             'monProfilForm'=>$monProfilForm ->createView()
         ]);
 
-        //return $this->render('participant/modifier.html.twig');
-
-
-        //return $this->render('mon_profil/modifier.html.twig', [
+        //return $this->render('participant/modifier.html.twig', [
         //    'controller_name' => 'MonProfilController',
         //]);
     }
--- src/Form/ParticipantType.php
+++ src/Form/ParticipantType.php
@@ -5,23 +5,49 @@ namespace App\Form;
 use App\Entity\Campus;
 use App\Entity\Participant;
 use App\Repository\CampusRepository;
+use Doctrine\DBAL\Types\TextType;
+use phpDocumentor\Reflection\Types\String_;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
 
 class ParticipantType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
+            /*->add('nom', TextType::class,
+                [
+                    'label' => 'Nom',
+                    'placeholder' => "votre nom svp",
+                ])*/
             ->add('nom')
             ->add('prenom')
             ->add('telephone')
             ->add('mail')
-            //->add('motPasse')
-            ->add('administrateur')
-            ->add('actif')
+            //->add('password')
+            ->add('mdp',null,
+                [
+                    'label' => 'Mot de passe',
+                    'mapped' => false,
+                ]
+            )
+            ->add('mdp2',null,
+                [
+                    'label' => 'Confirmez le mot de passe',
+                    'mapped' => false,
+                ]
+            )
+            /*->add('mdp', Participant::class,
+                [
+                    //'label' => 'motDePasse',
+                    'mapped' => false,
+                ]
+            )*/
+            //->add('administrateur')
+            //->add('actif')
             ->add('pseudo')
             ->add('sortiesInscrit')
             ->add('getEstRattacheA', EntityType::class, [
@@ -30,7 +56,7 @@ class ParticipantType extends AbstractType
                 'placeholder'=>'saisir un campus',
                 'query_builder'=>function(CampusRepository $campusRepository)
                 {
-                    return $campusRepository->createQueryBuilder('nom');
+                    return $campusRepository->createQueryBuilder('c')->orderBy('c.nom','ASC');
                     //return $campusRepository->createQueryBuilder('name')->orderBy('nom','ASC');
                 }
             ])
